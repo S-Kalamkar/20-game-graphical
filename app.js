9caff5628daa2293f473fbf02b8a54fccf1c0f8e
Kalamkar <skalamkar1@FVFZMYB5J1WK.tld>
2022-04-14 13:15:48 -0500

    card deck fix

--- app.js
+++ app.js
@@ -5,37 +5,14 @@ function run() {
 
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 9c6fadd99a75821a2352859d98e3f3f48f8fc94c
-=======
->>>>>>> 49197a2fad0ac798f39d1cff40dc957c9e47f865
 function restart() {
     location.reload();
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-function getCard() {
-    var r = Math.floor(Math.random() *10 ) + 1
-    if (r==1){
-        r = "A"
-    }
-=======
->>>>>>> 9c6fadd99a75821a2352859d98e3f3f48f8fc94c
-function ranNum() {
-    var rng = Math.floor(Math.random() *10) +1
-    // console.log(rng)
-    return rng
-=======
 function ranNum() {
     var rng = Math.floor(Math.random() *10) +1
     console.log(rng);
     return rng;
->>>>>>> 49197a2fad0ac798f39d1cff40dc957c9e47f865
 }
 
 function cardType(num) {
@@ -55,11 +32,7 @@ function cardType(num) {
         value="A";
     }
     // console.log(value)
-<<<<<<< HEAD
-    return value
-=======
     return value;
->>>>>>> 49197a2fad0ac798f39d1cff40dc957c9e47f865
 }
 
 function cardSuit() {
@@ -73,13 +46,8 @@ function cardSuit() {
     } else {
         rSuit="S";
     }
-<<<<<<< HEAD
-    // console.log(rSuit)
-    return rSuit
-=======
     console.log(rSuit);
     return rSuit;
->>>>>>> 49197a2fad0ac798f39d1cff40dc957c9e47f865
 }
 
 
@@ -90,40 +58,13 @@ function cardMake(type, suit) {
 }
 
 function computerPlay(){
-<<<<<<< HEAD
-    var comNum = 0
-    var talkDiv=document.getElementById("gameTalk")
-    talkDiv.innerHTML = "The computer will go now!"
-    var randnum = ranNum()
-    var type = cardType(randnum)
-    var suit = cardSuit()
-    var card = cardMake(type, suit)
-    
-    do {
-        var cardpull = 0
-        cardpull++
-
-        randnum = ranNum()
-        type = cardType(randnum)
-        suit = cardSuit()
-        card = cardMake(type, suit)
-        comNum = comNum + randnum
-        console.log(randnum)
-        document.getElementById("card").innerHTML = card
-        
-    } while (comNum < 16)
-
-    talkDiv.innerHTML = `The computer got ${comNum}`
-    return comNum
-}
-=======
     var comNum = 0;
     var talkDiv = document.getElementById("gameTalk");
     talkDiv.innerHTML = "The computer will go now!";
     var randNum = 0;
     var card = 0;
     var cards = [];
-    
+
     do {
         randNum = ranNum();
         console.log(card);
@@ -132,27 +73,15 @@ function computerPlay(){
         document.getElementById("card").innerHTML = cards;
         comNum = comNum + randNum;
         console.log(comNum);
-        
+
 
     } while(comNum < 16)
-    
+
     talkDiv.innerHTML = `The computer got ${comNum}!`;
->>>>>>> 49197a2fad0ac798f39d1cff40dc957c9e47f865
 
     return comNum;
 }
 
-<<<<<<< HEAD
-=======
-computerPlay()
-<<<<<<< HEAD
-// console.log(computerPlay())
-=======
-console.log(computerPlay())
->>>>>>> aebe8fff6049f1c95ced97e37a696d4dd05fd1f9
->>>>>>> 9c6fadd99a75821a2352859d98e3f3f48f8fc94c
-=======
 function userPlay(){
-    
+
 }
->>>>>>> 49197a2fad0ac798f39d1cff40dc957c9e47f865
